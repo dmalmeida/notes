643aac4aa6586e3f3493720cb67cf31018504643
David Almeida <david.miguel.almeida@gmail.com>
2021-09-09 11:22:47 +0100

    Primeira versao

---
+++ lib/views/note_list.dart
@@ -0,0 +1,75 @@
+﻿import 'package:flutter/material.dart';
+import 'package:notes/models/note_for_listing.dart';
+import 'package:notes/views/note_delete.dart';
+import 'package:notes/views/note_modify.dart';
+
+class NoteList extends StatelessWidget {
+  final notes = [
+    new NoteForListing(
+        noteID: "1",
+        createDateTime: DateTime.now(),
+        lastestEditDateTime: DateTime.now(),
+        noteTitle: "Note 1"),
+    new NoteForListing(
+        noteID: "2",
+        createDateTime: DateTime.now(),
+        lastestEditDateTime: DateTime.now(),
+        noteTitle: "Note 2"),
+    new NoteForListing(
+        noteID: "3",
+        createDateTime: DateTime.now(),
+        lastestEditDateTime: DateTime.now(),
+        noteTitle: "Note 3"),
+  ];
+
+  String formatDateTime(DateTime dateTime) {
+    return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('List of notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => NoteModify(noteID: 'Create')));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: ListView.separated(
+          itemBuilder: (_, index) {
+            return Dismissible(
+              key: ValueKey(notes[index].noteID),
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction) {},
+              confirmDismiss: (direction) async {
+                final result = await showDialog(
+                  context: context,
+                  builder: (_) => NoteDelete(),
+                );
+                print(result);
+                return result;
+              },
+              child: ListTile(
+                title: Text(
+                  notes[index].noteTitle,
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+                subtitle: Text(
+                    'Last edited on ${formatDateTime(notes[index].lastestEditDateTime)}'),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => NoteModify(noteID: notes[index].noteID)));
+                },
+              ),
+            );
+          },
+          separatorBuilder: (_, __) => Divider(
+                height: 1,
+                color: Colors.green,
+              ),
+          itemCount: notes.length),
+    );
+  }
+}
---
+++ lib/views/note_modify.dart
@@ -0,0 +1,44 @@
+﻿import 'package:flutter/material.dart';
+
+class NoteModify extends StatelessWidget {
+  final String noteID;
+  NoteModify({required this.noteID});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title:
+              Text(noteID == 'Create' ? 'Create note' : 'Edit note: $noteID')),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Note Title',
+              ),
+            ),
+            Container(height: 8),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Note Content',
+              ),
+            ),
+            Container(height: 16),
+            SizedBox(
+              width: double.infinity,
+              height: 35,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Submit'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
